a36969e374c889cd19d6def21fbaf4524865ce76
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-04-25 13:42:46 +0200

    Initial commit
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugin.go
+++ plugin.go
@@ -1,8 +1,19 @@
 package otel
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/roadrunner-server/api/v2/plugins/config"
 	"github.com/roadrunner-server/errors"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
@@ -11,8 +22,10 @@ const (
 )
 
 type Plugin struct {
-	cfg *Config
-	log *zap.Logger
+	cfg    *Config
+	log    *zap.Logger
+	tracer trace.Tracer
+	client otlptrace.Client
 }
 
 func (p *Plugin) Init(cfg config.Configurer, log *zap.Logger) error {
@@ -30,5 +43,28 @@ func (p *Plugin) Init(cfg config.Configurer, log *zap.Logger) error {
 	p.log = &zap.Logger{}
 	*p.log = *log
 
+	p.client = otlptracehttp.NewClient()
+	p.tracer = otel.GetTracerProvider().Tracer("foo", trace.WithInstrumentationVersion("v0.1.0"), trace.WithSchemaURL(semconv.SchemaURL))
+	exporter, err := otlptrace.New(context.Background(), p.client)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(newResource()),
+	)
+
+	otel.SetTracerProvider(tracerProvider)
+
 	return nil
+}
+
+func (p *Plugin) Middleware(next http.Handler) http.Handler {
+	//ctx, span := tracer.Start()
+	return otelhttp.NewHandler(next, "rr-request")
+}
+
+func newResource() *resource.Resource {
+	return resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String("otlptrace-example"),
+		semconv.ServiceVersionKey.String("0.0.1"),
+	)
 }
