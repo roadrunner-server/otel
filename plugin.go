195c1d79ba51c28ffe40d880a9cf0566d3ce7dc5
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-02-03 17:42:57 +0100

    chore: update semconv to v1.39
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugin.go
+++ plugin.go
@@ -18,7 +18,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
 	"go.temporal.io/sdk/interceptor"
 	"go.uber.org/zap"
 
